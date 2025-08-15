9593825b90d9cc9ae33672b71ab37b71cb0bed32
Elena Polyakova <epolyk1970@gmail.com>
2025-08-15 16:14:18 +0300

    add

--- app/notes/filter/page.tsx
+++ app/notes/filter/page.tsx
@@ -1,9 +1,10 @@
-
 import { fetchNotes } from '@/lib/api';
 import NoteList from '@/components/NoteList/NoteList';
 
+export const dynamic = 'force-dynamic';
+
 export default async function NotesFilterPage() {
-  const { results: notes } = await fetchNotes({}); 
+  const { results: notes } = await fetchNotes({});
 
   return (
     <div>
